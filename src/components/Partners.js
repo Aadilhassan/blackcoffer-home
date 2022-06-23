02c2ef78ea4cf0664b0f88428098845b5d332094
Aadil Hassan <aahanalhassan@gmail.com>
2022-06-23 20:57:05 +0530

    Academia partners

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,35 +1,60 @@
 import React from 'react'
 
+import './Partners.css'
 export default function Partners() {
 
+
+   
   
   return (
     <>
     <h1 style={{textAlign:'center'}}>Business Partners
 </h1>
-    <div style={{display:'flex', flexWrap:'wrap', maxWidth: '100vw'}}>
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/9-5.png' width={500} height={250} />
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/10-5.png' width={500} height={250} />
-   
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/6-5.png' width={500} height={250} />
-   
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/3-5.png' width={500} height={250} />
-   
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/11-5.png' width={500} height={250} />
-   
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/2-5.png' width={500} height={250} />
-   
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/1-6.png' width={500} height={250} />
-   
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/7-5.png' width={500} height={250} />
-   
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/5-5.png' width={500} height={250} />
-   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/7-5.png' width={500} height={250} />
+
+  
+ 
+    <div id='wraper' >
 
 
+    <img src='https://blackcoffer.com/wp-content/uploads/2022/01/9-5.png' id='img' />
+   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/10-5.png' id='img' />
+   
+   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/6-5.png' id='img' />
+   
+   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/3-5.png' id='img' />
+   
+   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/11-5.png' id='img' />
+   
+   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/2-5.png' id='img' />
+   
+   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/1-6.png' id='img' />
+   
+   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/7-5.png' id='img' />
+   
+   <img src='https://blackcoffer.com/wp-content/uploads/2022/01/5-5.png' id='img' />
+  
+
+ 
 
 
     </div>
+    <h1 style={{textAlign:'center'}}>Academia Partners
+</h1>
+ <div id='wraper' >
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/3-4.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/15-2.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/14-2.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/13-2.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/12-2.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/11-4.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/10-4.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/8-4.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/7-4.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/6-4.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/5-4.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/4-4.png' id='img' />
+ <img src='https://blackcoffer.com/wp-content/uploads/2022/01/9-4.png' id='img' />
+</div>
     </>
   )
 }
